c67a273f063c5d76455284eb224eab60c7b22a48
ztucker4 <tuckerzoej@gmail.com>
2021-09-03 09:50:49 -0700

    SYS-291: Add 3 initial models and some config (#2)

--- lbs/qdb/models.py
+++ lbs/qdb/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Staff(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Units(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.ManyToManyField(Staff, through = 'Recipients')
+
+    def __str__(self):
+        return self.name
+
+class Recipients(models.Model):
+    unit = models.ForeignKey(Units, on_delete=models.CASCADE)
+    recipient = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100)
+
