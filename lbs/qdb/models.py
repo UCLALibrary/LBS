323fec95ea3767f88c6b7cd73166359c5752e8f5
ztucker4 <tuckerzoej@gmail.com>
2021-09-17 13:21:02 -0700

    SYS-590 - Create Accounts model (#4)
    * add account model
    * fixed Account model name, updated Account field lengths, and added __str__ methods

--- lbs/qdb/models.py
+++ lbs/qdb/models.py
@@ -19,8 +19,23 @@ class Recipient(models.Model):
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
     recipient = models.ForeignKey(Staff, on_delete=models.CASCADE)
     role = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.recipient
 
 class Subcode(models.Model):
     code = models.CharField(max_length=4)
     titles = models.TextField()
     notes = models.TextField()
+    
+    def __str__(self):
+        return self.code
+    
+class Accounts(models.Model):
+    account = CharField(max_length=6)
+    cost_center = CharField(max_length=2)
+    title = TextField()
+    unit = models.ForeignKey(Units)
+
+    def __str__(self):
+        return self.account
