e0f7a409fb6b1006f2c37ddac4bb6170fea426c7
ztucker4 <tuckerzoej@gmail.com>
2021-10-01 14:57:03 -0700

    formatting fixes for csv import

--- lbs/qdb/management/commands/load_initial_data.py
+++ lbs/qdb/management/commands/load_initial_data.py
@@ -6,7 +6,7 @@ from qdb.models import Staff, Unit, Accounts, Subcode, Recipient
 
 import csv
 
-def create_staff(self, staff_file):
+def populate_persons(self, staff_file):
     """
     Does initial creation of Staff
     """
@@ -23,9 +23,9 @@ def create_staff(self, staff_file):
             self.stdout.write(f"\tProcessing {name}...")
             
             # Staff
-            staff = _get_staff(staff_id, name, email)
+            person = _create_person(staff_id, name, email)
 
-def create_unit(self, unit_file):
+def populate_units(self, unit_file):
     """
     Does initial creation of Units
     """
@@ -41,10 +41,10 @@ def create_unit(self, unit_file):
 
             self.stdout.write(f"\tProcessing {name}...")
 
-            # Units
-            unit = _get_unit(name, head, aul)
+            # Library Units
+            unit = _create_unit(name, head, aul)
 
-def create_accounts(self, accounts_file):
+def populate_accounts(self, accounts_file):
     """
     Does initial creation of Accounts
     """
@@ -54,7 +54,7 @@ def create_accounts(self, accounts_file):
         
         for row in reader:
             # Get relevant data from CSV
-            account = row["account"]
+            account_num = row["account"]
             cost_center = row["cost_center"]
             title = row["title"]
             unit_id = row["unit"]
@@ -62,13 +62,13 @@ def create_accounts(self, accounts_file):
             self.stdout.write(f"\tProcessing {title}...")
 
             # Accounts
-            account_db = _get_account(account, cost_center, title, unit_id)
+            account = _create_account(account_num, cost_center, title, unit_id)
 
-def create_subcodes(self):
+def populate_subcodes(self):
     """Does initial subcode creation"""
     self.stdout.write(f"Adding subcodes")
     #just hardcoding it for now
-    SUBCODES = {
+    subcode_dict = {
             '00': {
                     'title':'Salaries - Academic',
                     'notes': 'LBS will balance sub 00 fund 19900 at year end'},
@@ -103,46 +103,46 @@ def create_subcodes(self):
                     'title': 'Overhead (F&A)',
                     'notes': 'Apply to Contracts and Grants'}
     }
-    for subcode in SUBCODES:
+    for subcode in subcode_dict:
         sc, created = Subcode.objects.get_or_create(code = subcode,
-                                            titles = SUBCODES[subcode]['title'],
-                                            notes = SUBCODES[subcode]['notes'])
+                                                    titles = subcode_dict[subcode]['title'],
+                                                    notes = subcode_dict[subcode]['notes'])
         sc.save()
         
     
             
 
-def _get_staff(staff_id, name, email):
-    # Users might already exist, via small initial load
+def _create_person(staff_id, name, email):
+    # Staff (persons) might already exist, via small initial load
     staff, created = Staff.objects.get_or_create(staff_id=staff_id, name=name,
                                                  email=email)
     staff.save()
     return staff
 
-def _get_unit(name, head, aul):
+def _create_unit(name, head, aul):
     # units might already exist
     unit, created = Unit.objects.get_or_create(name=name)
     if head:
-        member1 = Staff.objects.get(staff_id=head)
-        unit.members.add(member1)
+        member = Staff.objects.get(staff_id=head)
+        unit.members.add(member)
         unit.save()
         #add roles to recipient table at this step
-        recip = Recipient.objects.get(recipient = member1, unit = unit)
+        recip = Recipient.objects.get(recipient = member, unit = unit)
         recip.role = "head"
         recip.save()
     if aul:
-        member2 = Staff.objects.get(staff_id=aul)
-        unit.members.add(member2)
+        member = Staff.objects.get(staff_id=aul)
+        unit.members.add(member)
         unit.save()
         #add roles to recipient table at this step
-        recip = Recipient.objects.get(recipient = member2, unit = unit)
+        recip = Recipient.objects.get(recipient = member, unit = unit)
         recip.role = "aul"
         recip.save()        
 
     unit.save()
     return unit
 
-def _get_account(acct, cost_center, title, unit_id):
+def _create_account(acct, cost_center, title, unit_id):
     #get foreign key first
     acct_unit = Unit.objects.get(id=unit_id)
     row, created = Accounts.objects.get_or_create(account=acct,
@@ -164,8 +164,8 @@ class Command(BaseCommand):
         staff = options["staff_file"]
         unit = options["unit_file"]
         accounts = options["accounts_file"]
-        create_staff(self, staff)
-        create_unit(self, unit)
-        create_accounts(self, accounts)
-        create_subcodes(self)
+        populate_persons(self, staff)
+        populate_units(self, unit)
+        populate_accounts(self, accounts)
+        populate_subcodes(self)
 
