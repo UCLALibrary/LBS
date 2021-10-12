ba9d2b7001b526e073f07a946107612148c2a066
Darrow Cole <darrowcoucla_03@darrow.com>
2021-10-12 14:21:21 -0700

    configured admin.py to route the admin interface (#8)
    Co-authored-by: Darrow Cole <darrowco@library.ucla.edu>

--- lbs/qdb/admin.py
+++ lbs/qdb/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Accounts, Recipient, Staff, Unit
 
-# Register your models here.
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Accounts)
+class AccountsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Recipient)
+class RecipientAdmin(admin.ModelAdmin):
+    pass
+
