7ff7f74557f88f2d77c8d2f953f0db8d5f4382df
darrowcoucla <darrowco@library.ucla.edu>
2021-10-20 11:49:21 -0700

    changed the model to create new migrations which implemented the changes

--- lbs/qdb/management/commands/load_initial_data.py
+++ lbs/qdb/management/commands/load_initial_data.py
@@ -2,9 +2,10 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
-from qdb.models import Staff, Unit, Accounts, Subcode, Recipient 
+from qdb.models import Staff, Unit, Account, Subcode, Recipient
 
 import csv
+
 
 def populate_persons(self, staff_file):
     """
@@ -13,17 +14,18 @@ def populate_persons(self, staff_file):
     self.stdout.write(f"Parsing {staff_file}")
     with open(staff_file, encoding="utf-8-sig", newline="") as csvfile:
         reader = csv.DictReader(csvfile, dialect="excel")
-        
+
         for row in reader:
             # Get relevant data from CSV
             staff_id = row["id"]
             name = row["name"]
             email = row["email"]
-            
+
             self.stdout.write(f"\tProcessing {name}...")
-            
+
             # Staff
             person = _create_person(staff_id, name, email)
+
 
 def populate_units(self, unit_file):
     """
@@ -32,7 +34,7 @@ def populate_units(self, unit_file):
     self.stdout.write(f"Parsing {unit_file}")
     with open(unit_file, encoding="utf-8-sig", newline="") as csvfile:
         reader = csv.DictReader(csvfile, dialect="excel")
-        
+
         for row in reader:
             # Get relevant data from CSV
             name = row["name"]
@@ -44,6 +46,7 @@ def populate_units(self, unit_file):
             # Library Units
             unit = _create_unit(name, head, aul)
 
+
 def populate_accounts(self, accounts_file):
     """
     Does initial creation of Accounts
@@ -51,7 +54,7 @@ def populate_accounts(self, accounts_file):
     self.stdout.write(f"Parsing {accounts_file}")
     with open(accounts_file, encoding="utf-8-sig", newline="") as csvfile:
         reader = csv.DictReader(csvfile, dialect="excel")
-        
+
         for row in reader:
             # Get relevant data from CSV
             account_num = row["account"]
@@ -61,56 +64,55 @@ def populate_accounts(self, accounts_file):
 
             self.stdout.write(f"\tProcessing {title}...")
 
-            # Accounts
+            # Account
             account = _create_account(account_num, cost_center, title, unit_id)
+
 
 def populate_subcodes(self):
     """Does initial subcode creation"""
     self.stdout.write(f"Adding subcodes")
-    #just hardcoding it for now
+    # just hardcoding it for now
     subcode_dict = {
-            '00': {
-                    'title':'Salaries - Academic',
-                    'notes': 'LBS will balance sub 00 fund 19900 at year end'},
-            '01': {
-                    'title': 'Salaries - Staff',
-                    'notes': 'LBS will balance sub 01 fund 19900 at year end'},
-            '02': {
-                    'title': 'General Assistance',
-                    'notes': 'Please use other (sub 02 only) LBS monthly report to track the allocations and expenditures'},
-            '03': {
-                    'title': 'Supplies and Expense',
-                    'notes': 'Includes travel'},
-            '04': {
-                    'title': 'Equipment and Facilities',
-                    'notes': ''},
-            '05': {
-                    'title': 'Books/Collections',
-                    'notes': ''},
-            '06': {
-                    'title': 'Employee Benefits',
-                    'notes': '19900 benefits will be funded from the Library Reserve account at year end'},
-            '07': {
-                    'title': 'Special Items',
-                    'notes': ''},
-            '08': {
-                    'title': 'Unallocated Funds',
-                    'notes': ''},
-            '09': {
-                    'title': 'Recharges and Departments',
-                    'notes': ''},
-            '9H': {
-                    'title': 'Overhead (F&A)',
-                    'notes': 'Apply to Contracts and Grants'}
+        '00': {
+            'title': 'Salaries - Academic',
+            'notes': 'LBS will balance sub 00 fund 19900 at year end'},
+        '01': {
+            'title': 'Salaries - Staff',
+            'notes': 'LBS will balance sub 01 fund 19900 at year end'},
+        '02': {
+            'title': 'General Assistance',
+            'notes': 'Please use other (sub 02 only) LBS monthly report to track the allocations and expenditures'},
+        '03': {
+            'title': 'Supplies and Expense',
+            'notes': 'Includes travel'},
+        '04': {
+            'title': 'Equipment and Facilities',
+            'notes': ''},
+        '05': {
+            'title': 'Books/Collections',
+            'notes': ''},
+        '06': {
+            'title': 'Employee Benefits',
+            'notes': '19900 benefits will be funded from the Library Reserve account at year end'},
+        '07': {
+            'title': 'Special Items',
+            'notes': ''},
+        '08': {
+            'title': 'Unallocated Funds',
+            'notes': ''},
+        '09': {
+            'title': 'Recharges and Departments',
+            'notes': ''},
+        '9H': {
+            'title': 'Overhead (F&A)',
+            'notes': 'Apply to Contracts and Grants'}
     }
     for subcode in subcode_dict:
-        sc, created = Subcode.objects.get_or_create(code = subcode,
-                                                    titles = subcode_dict[subcode]['title'],
-                                                    notes = subcode_dict[subcode]['notes'])
+        sc, created = Subcode.objects.get_or_create(code=subcode,
+                                                    titles=subcode_dict[subcode]['title'],
+                                                    notes=subcode_dict[subcode]['notes'])
         sc.save()
-        
-    
-            
+
 
 def _create_person(staff_id, name, email):
     # Staff (persons) might already exist, via small initial load
@@ -119,6 +121,7 @@ def _create_person(staff_id, name, email):
     staff.save()
     return staff
 
+
 def _create_unit(name, head, aul):
     # units might already exist
     unit, created = Unit.objects.get_or_create(name=name)
@@ -126,31 +129,33 @@ def _create_unit(name, head, aul):
         member = Staff.objects.get(staff_id=head)
         unit.members.add(member)
         unit.save()
-        #add roles to recipient table at this step
-        recip = Recipient.objects.get(recipient = member, unit = unit)
+        # add roles to recipient table at this step
+        recip = Recipient.objects.get(recipient=member, unit=unit)
         recip.role = "head"
         recip.save()
     if aul:
         member = Staff.objects.get(staff_id=aul)
         unit.members.add(member)
         unit.save()
-        #add roles to recipient table at this step
-        recip = Recipient.objects.get(recipient = member, unit = unit)
+        # add roles to recipient table at this step
+        recip = Recipient.objects.get(recipient=member, unit=unit)
         recip.role = "aul"
-        recip.save()        
+        recip.save()
 
     unit.save()
     return unit
 
+
 def _create_account(acct, cost_center, title, unit_id):
-    #get foreign key first
+    # get foreign key first
     acct_unit = Unit.objects.get(id=unit_id)
-    row, created = Accounts.objects.get_or_create(account=acct,
-                                           cost_center=cost_center,
-                                           title=title,
-                                           unit = acct_unit)
+    row, created = Account.objects.get_or_create(account=acct,
+                                                 cost_center=cost_center,
+                                                 title=title,
+                                                 unit=acct_unit)
     row.save()
     return row
+
 
 class Command(BaseCommand):
     help = "Load QDB data from CSV into database"
@@ -168,4 +173,3 @@ class Command(BaseCommand):
         populate_units(self, unit)
         populate_accounts(self, accounts)
         populate_subcodes(self)
-
--- lbs/qdb/models.py
+++ lbs/qdb/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Staff(models.Model):
     staff_id = models.IntegerField()
     name = models.CharField(max_length=100)
@@ -8,30 +9,34 @@ class Staff(models.Model):
     def __str__(self):
         return self.name
 
+
 class Unit(models.Model):
     name = models.CharField(max_length=100)
-    members = models.ManyToManyField(Staff, through = 'Recipient')
+    members = models.ManyToManyField(Staff, through='Recipient')
 
     def __str__(self):
         return self.name
+
 
 class Recipient(models.Model):
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
     recipient = models.ForeignKey(Staff, on_delete=models.CASCADE)
     role = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.recipient
+
 
 class Subcode(models.Model):
     code = models.CharField(max_length=4)
     titles = models.TextField()
     notes = models.TextField()
-    
+
     def __str__(self):
         return self.code
-    
-class Accounts(models.Model):
+
+
+class Account(models.Model):
     account = models.CharField(max_length=6)
     cost_center = models.CharField(max_length=2)
     title = models.TextField()
