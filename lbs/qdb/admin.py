0c909e7a5b10d38e928dc187d90e05282c229bf1
Darrow Cole <darrowco@library.ucla.edu>
2021-10-20 13:49:47 -0700

    SYS-624: Fix model plural problems (#9)
    * set Accounts to Account
    * set Staff plural to Staff
    * created migrations for change from Staffs to Staff
    * removed initial Accounts update
    * changed the model to create new migrations which implemented the changes

--- lbs/qdb/admin.py
+++ lbs/qdb/admin.py
@@ -1,19 +1,22 @@
 from django.contrib import admin
-from .models import Accounts, Recipient, Staff, Unit
+from .models import Account, Recipient, Staff, Unit
+
 
 @admin.register(Staff)
 class StaffAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
     pass
 
-@admin.register(Accounts)
-class AccountsAdmin(admin.ModelAdmin):
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Recipient)
 class RecipientAdmin(admin.ModelAdmin):
     pass
-
