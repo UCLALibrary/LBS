fa60db47e5724b617e09fa88002969dba0b6bcc3
darrowcoucla <darrowco@library.ucla.edu>
2021-10-21 10:29:40 -0700

    added admin display code

--- lbs/qdb/admin.py
+++ lbs/qdb/admin.py
@@ -4,19 +4,34 @@ from .models import Account, Recipient, Staff, Unit
 
 @admin.register(Staff)
 class StaffAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name', 'email')
+    ordering = ('name',)
+    search_fields = ('name', 'email')
 
 
 @admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name',)
+    ordering = ('name',)
+    search_fields = ('name',)
 
 
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('account', 'title', 'cost_center')
+    ordering = ('title',)
+    search_fields = ('title',)
 
 
 @admin.register(Recipient)
 class RecipientAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('recipient_id', 'name', 'unit_id', 'unit', 'role')
+    ordering = ('recipient_id', 'role',)
+
+    def name(self, obj):
+        result = Staff.objects.filter(staff_id=getattr(obj, "recipient_id"))[0]
+        return result
+
+    def unit(self, obj):
+        result = Staff.objects.filter(unit_id=getattr(obj, "recipient_id"))[0]
+        return result
