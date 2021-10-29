0df2c7c4da14614f3fa6ee00acb5251dc6d6cee6
ztucker4 <tuckerzoej@gmail.com>
2021-10-29 10:52:54 -0700

    Add draft tests of models

--- lbs/qdb/tests.py
+++ lbs/qdb/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+from .models import Staff, Unit, Recipient, Subcode, Account
+
+
+class ModelsTestCase(TestCase):
+    
+    fixtures = ["sample_data.json"]
+
+    def test_staff(self):
+        staff = Staff.objects.get(pk=1)
+        self.assertEqual(str(staff), 'Sharon Farb')
+        self.assertEqual(staff.name, 'Sharon Farb')
+        self.assertEqual(staff.email, 'farb@library.ucla.edu')
+        
+
+    def test_unit(self):
+        unit = Unit.objects.get(pk=1)
+        self.assertEqual(str(unit), 'Oral History')
+        self.assertEqual(unit.name, 'Oral History')
+
+    def test_subcode(self):
+        subcode = Subcode.objects.get(pk=1)
+        self.assertEqual(str(subcode), '00')
+        self.assertEqual(subcode.titles, 'Salaries - Academic')
+        self.assertEqual(subcode.notes,
+                         'LBS will balance sub 00 fund 19900 at year end')
+
+    def test_account(self):
+        account = Account.objects.get(pk=2)
+        self.assertEqual(str(account), '436000')
+        self.assertEqual(account.account, '436000')
+        self.assertEqual(account.cost_center, 'BF')
+        self.assertEqual(account.title,
+                         'LIBRARY:COLL DEV-ORAL HSTR/BUS FORECAST')
+        self.assertEqual(account.unit_id, 1)
+
+>>>>>>> Stashed changes
