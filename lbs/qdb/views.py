3271e2b21ecbb8e936d6ad3dbe7a4082c1ec33fb
darrowcoucla <darrowco@library.ucla.edu>
2021-12-08 09:49:10 -0800

    working report generation button; drop old db, create reports dir before running

--- lbs/qdb/views.py
+++ lbs/qdb/views.py
@@ -1,40 +1,32 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
-from .forms import ReportForm
+from django.http.response import HttpResponse
 from django.core.management import call_command
 from django.contrib.auth.decorators import login_required
 import os
 from .models import Unit
+from .forms import ReportForm
 
 
 def report(request):
-    objectlist = Unit.objects.values('name').distinct().order_by('name')
     if request.method == 'POST':
-        form = ReportForm(request.POST)
+        submitbutton = request.POST.get("submit")
+        form = ReportForm(request.POST or None)
         if form.is_valid():
-            ###objectlist = Unit.objects.distinct('name')
-            unit = form.cleaned_data['unit']
-            date = form.cleaned_data['date']
-            ###print(unit, date)
-            report = run_qdb_reporter(unit)
+            unit = form.cleaned_data['unit'].id
+            year = form.cleaned_data['year']
+            month = form.cleaned_data['month']
+            report = run_qdb_reporter(unit, month, year)
 
-    form = ReportForm()
-    return render(request, 'form.html', {'form': form, 'objectlist': objectlist})
-    # return HttpResponse('report view')
+        context = {'form': form, 'unit': unit, 'month': month,
+                   'year': year, 'submitbutton': submitbutton}
+
+        return render(request, 'form.html', context)
+    else:
+        form = ReportForm()
+        return render(request, 'form.html', {'form': form})
 
 
-def run_qdb_reporter(unit_from_form):
-    print(unit_from_form)
-
-    ###make_option('-l', dest='l', action='store_true', help='list units')
-
-    print(os.environ['QDB_DB_PASSWORD'])
-
-    ###call_command('run_qdb_reporter', list_units=True)
-    call_command('run_qdb_reporter', year=2021,
-                 month=5, units=[6], list_units=True)
-    ###call_command('artifact_db_loader', 'artefacts', tzsub=True, e=True)
-
-    # return subprocess.run(['python', 'qdb/management/commands/run_qdb_reporter.py', unit_from_form], shell=False, timeout=1800)
-    # return subprocess.run(['python', 'windows_path_like_D:\\path_to_script\\prog17.py', post_from_form], shell=False, timeout=1800)
+def run_qdb_reporter(unit_from_form, month_from_form, year_from_form):
+    call_command('run_qdb_reporter', year=int(year_from_form),
+                 month=int(month_from_form), units=[unit_from_form], list_units=True)
+    # append , list_units=True for testing
