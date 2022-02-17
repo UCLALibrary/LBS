cd0d3522369399b0942e067b315edbc34c64dd7b
darrowcoucla <darrowco@library.ucla.edu>
2022-02-17 11:18:35 -0800

    rewrote ajax to fix missing message, added messaging framework and more - see PR notes

--- lbs/static/js/main.js
+++ lbs/static/js/main.js
@@ -1,32 +1,74 @@
 const spinnerBox = document.getElementById('spinner-box')
 const longBox = document.getElementById('long-box')
-const ajaxBox = document.getElementById('ajax-box')
 var message_box = document.getElementById("error_message");
 
-$(function () {
-    $('#report_submit').on('click', function () {
-        $.ajax({
-            type: 'GET',
-            url: '/qdb/report/',
-            success: function(response){
-                setTimeout(()=>{
-                    spinnerBox.classList.remove("not-visible")
-                    var unit_selected = $("#id_unit option:selected").text();
-                    if (unit_selected == 'All units'){
-                        longBox.classList.remove("not-visible");
-                    }
+const form = document.getElementById('p-form')
 
-                }, 500)
+const unit = document.getElementById('id_unit')
+const year = document.getElementById('id_year')
+const month = document.getElementById('id_month')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
+
+const url = ""
+
+$(function () {
+    form.addEventListener('submit', e=>{
+        // close any old messages on new submissioms
+        $(".alert").alert('close');
+
+        // start spinner on submissiom
+        spinnerBox.classList.remove("not-visible");
+
+        // prevent the default submission of the form
+        e.preventDefault();
+
+        // collect the form's data
+        const formData = new FormData()
+        formData.append('csrfmiddlewaretoken', csrf[0].value)
+        formData.append('unit', unit.value)
+        formData.append('year', year.value)
+        formData.append('month', month.value)
+
+        // display "long wait" warning if All units was specified
+        var unit_selected = $("#id_unit option:selected").text();
+        if (unit_selected == 'All units'){
+            longBox.classList.remove("not-visible");
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: formData,
+            success: function(data){
+                success = data.success;
+
+                // turn off spinner and any "long wait" warning
+                spinnerBox.classList.add("not-visible");
+                var unit_selected = $("#id_unit option:selected").text();
+                if (unit_selected == 'All units'){
+                    longBox.classList.add("not-visible");
+                }
+
+                // pass any messages to js in form.html
+                update_messages(data.messages);
             },
             error: function(data) {
-                setTimeout(()=>{
-                    ajaxBox.classList.remove("not-visible")
-                }, 500)
+                // turn off spinner and any "long wait" warning
+                spinnerBox.classList.add("not-visible");
+                var unit_selected = $("#id_unit option:selected").text();
+                if (unit_selected == 'All units'){
+                    longBox.classList.add("not-visible");
+                }
+                // display ajax error
+                $("#ajax-box").append("<div class='alert alert-danger alert-dismissible fade show' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><center>AJAX Error: no report could be generated.<br>-----<br>Please report this to the DIIT Help Desk:<br><a href='https://jira.library.ucla.edu/servicedesk/customer/portals'>UCLA Library Service Portal</a></center></div>");
             },
+            cache: false,
+            contentType: false,
+            processData: false
         })
-        message_box.style.display = "none";
     });
 });
+
 // move the "All units" option to the top of the select
 $(document).ready( function () {
     const optionText = 'All units';
