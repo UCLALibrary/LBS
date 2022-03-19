a096eec3717a43b26e286dd340a7aaab5ce258b7
Andy Kohler <akohler@library.ucla.edu>
2022-03-18 17:16:00 -0800

    SYS-707: Remove docker-compose secret management, simplify dev and GA/CI

--- lbs/qdb/scripts/settings.py
+++ lbs/qdb/scripts/settings.py
@@ -1,17 +1,5 @@
 import os
 
-# Support docker secret files by reading the secret from file
-# https://en.ovcharov.me/2021/09/30/use-docker-secrets-in-django/
-
-
-def get_secret(key, default):
-    value = os.getenv(key, default)
-    if os.path.isfile(value):
-        with open(value) as f:
-            return f.read().strip()
-    return value
-
-
 # Environment
 # Can be 'dev' or 'prod'
 ENV = os.environ.get('DJANGO_RUN_ENV', 'dev')
@@ -30,7 +18,7 @@ PASSWORD = os.environ['QDB_PASSWORD']
 DB_SERVER = os.environ['QDB_DB_SERVER']
 DB_DATABASE = os.environ['QDB_DB_DATABASE']
 DB_USER = os.environ['QDB_DB_USER']
-DB_PASSWORD = get_secret('QDB_DB_PASSWORD_FILE', '')
+DB_PASSWORD = os.environ.get('QDB_DB_PASSWORD', '') # not relevant for CI builds
 
 # Folder for reports
 REPORTS_DIR = os.path.join(BASE_DIR, 'reports')
