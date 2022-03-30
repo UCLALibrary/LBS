5e763fa3ddb2d418aed037dffe45ffcba3399aaf
darrowcoucla <darrowco@library.ucla.edu>
2022-03-30 09:55:15 -0700

    updated, reduced code with functions; updated variable names

--- lbs/qdb/admin.py
+++ lbs/qdb/admin.py
@@ -19,18 +19,19 @@ class UnitAdmin(admin.ModelAdmin):
     ordering = ('name',)
     search_fields = ('name',)
 
+    # one method per list item for unit_aul, unit_head, unit_assoc
     def unit_aul(self, obj):
-        result = edit_recip_link(obj, "aul")
-        return mark_safe(result)
+        result = mark_safe(get_recipient_link(obj, "aul"))
+        return result
 
     def unit_head(self, obj):
-        result = edit_recip_link(obj, "head")
-        return mark_safe(result)
+        result = mark_safe(get_recipient_link(obj, "head"))
+        return result
 
     # dhc: put these into a for loop
     def unit_assoc(self, obj):
-        result = edit_recip_link(obj, "assoc")
-        return mark_safe(result)
+        result = mark_safe(get_recipient_link(obj, "assoc"))
+        return result
 
 
 # create page to display accounts
@@ -56,30 +57,42 @@ class RecipientAdmin(admin.ModelAdmin):
         result = Staff.objects.filter(unit_id=getattr(obj, "recipient_id"))[0]
         return result
 
+
 # given unit, add aul, head and assoc recipients as links to edit the recipient
-
-
-def edit_recip_link(obj, unitRecipTitle):
+def get_recipient_link(obj, role):
     # if the current unit has a passed-in value which matches either aul, head, or assoc
-    if(Recipient.objects.filter(unit_id=getattr(obj, "id")) & Recipient.objects.filter(role=unitRecipTitle)):
-        # id from the staff table of the staff member who is the unitRecipTitle of the current unit
-        staff_id_unitRecipTitle = (Recipient.objects.filter(unit_id=getattr(
-            obj, "id")) & Recipient.objects.filter(role=unitRecipTitle)).values_list('recipient_id', flat=True)
-        # name from the staff table of the staff member who is the unitRecipTitle of the current unit
-        staff_name_unitRecipTitle = Staff.objects.filter(
-            id=staff_id_unitRecipTitle[0]).values_list('name', flat=True)
-        # id from the unit table of the current unit which has the staff member as the unitRecipTitle
-        unit_id_unitRecipTitle = (Recipient.objects.filter(unit_id=getattr(
-            obj, "id")) & Recipient.objects.filter(role=unitRecipTitle)).values_list('id', flat=True)
-        # return a link to manage the unit and role of the staff member currently designated unitRecipTitle of the current unit
+    if(Recipient.objects.filter(unit_id=getattr(
+            obj, "id"), role=role)):
+
+        # id from the staff table of the staff member who has the role of the current unit
+        staff_id_role = get_recipient_list(
+            obj, role, 'recipient_id')
+
+        # id from the unit table of the current unit which has the staff member who has the role of the current unit
+        unit_id_role = get_recipient_list(
+            obj, role, 'id')
+
+        # name from the staff table of the staff member who has the role of the current unit
+        staff_name = Staff.objects.filter(
+            id=staff_id_role[0]).values_list('name', flat=True)  # 70
+
+        # return a link to manage the unit and role of the staff member who has the role of the current unit
         result = "<a href=../../../admin/qdb/recipient/" + \
-            str(unit_id_unitRecipTitle[0]) + "/change/>" + \
-            str(staff_name_unitRecipTitle[0]) + "</a>"
+            str(unit_id_role[0]) + "/change/>" + \
+            str(staff_name[0]) + "</a>"
+        return result
     else:
         # id from the unit table of the current unit
         unit_id_num = getattr(obj, "id")
-        # return a link to add a recipient (staff member) and role to the current unit
+
+        # return a link to add a recipient (staff member) and role to the current unit which currently has none
         result = "<a href=../../../admin/qdb/recipient/add/?unit=" + \
             str(unit_id_num) + ">-----</a>"
+        return result
 
-    return(result)
+
+# return id given recipient type (aul, head or assoc) and column name of the value needed
+def get_recipient_list(obj, recipient_type, value_needed):
+    result = Recipient.objects.filter(unit_id=getattr(
+        obj, "id"), role=recipient_type).values_list(value_needed, flat=True)
+    return result
