06d69931cf92780c7e2b402674e1936f5b6efd7b
Andy Kohler <akohler@library.ucla.edu>
2022-06-03 15:40:42 -0800

    SYS-818: Add default route '/'

--- lbs/qdb/urls.py
+++ lbs/qdb/urls.py
@@ -1,8 +1,8 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('qdb/report/', views.report),
     path('qdb/logout/', views.logoutandlogin),
+    path('', views.report),
 ]
