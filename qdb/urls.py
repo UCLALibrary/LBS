784767ad9d1b044019e4b853b39c8f81970f52af
Andy Kohler <akohler@library.ucla.edu>
2023-09-22 16:57:51 -0800

    SYS-1407: Add release notes page

--- qdb/urls.py
+++ qdb/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('qdb/report/', views.report),
-    path('qdb/logout/', views.logoutandlogin),
-    path('', views.report),
+    path("qdb/report/", views.report),
+    path("qdb/logout/", views.logoutandlogin),
+    path("qdb/release_notes/", views.release_notes, name="release_notes"),
+    path("", views.report),
 ]
