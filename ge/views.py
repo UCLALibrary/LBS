be390dd2a60530d734e6e70d24f0f5c0793d1cbc
Andy Kohler <akohler@library.ucla.edu>
2023-10-05 10:17:56 -0800

    SYS-1415: Import Excel data (#75)
    * SYS-1415: Add ability to import Excel data
    
    * SYS-1412: Add form/view/template to support the import of Excel data
    
    * Add basic error handling and message display

--- ge/views.py
+++ ge/views.py
@@ -1,3 +1,29 @@
+from django.contrib import messages
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from ge.forms import ExcelUploadForm
+from ge.models import BFSImport, CDWImport, MTFImport
+from ge.views_utils import import_excel_data
 
-# Create your views here.
+
+@login_required
+def report(request: HttpRequest):
+    if request.method == "POST":
+        form = ExcelUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            bfs_file = request.FILES["bfs_filename"]
+            cdw_file = request.FILES["cdw_filename"]
+            mtf_file = request.FILES["mtf_filename"]
+            try:
+                import_excel_data(bfs_file, BFSImport)
+                import_excel_data(cdw_file, CDWImport)
+                import_excel_data(mtf_file, MTFImport)
+                messages.success(request, "All data was imported successfully.")
+            except KeyError as ex:
+                # Exception re-raised from import_excel_data has useful info already.
+                messages.error(request, ex)
+    else:
+        form = ExcelUploadForm()
+    context = {"form": form}
+    return render(request, "ge/ge_report.html", context)
