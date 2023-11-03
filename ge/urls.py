ddd8c3aca1ad8432d25ef9257d7d82a0ff6ff754
Andy Kohler <akohler@library.ucla.edu>
2023-11-03 09:18:28 -0800

    Add forms/views/templates to search and edit LibraryData records

--- ge/urls.py
+++ ge/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("ge/report/", views.report),
     path("", views.report),
+    path("ge/report/", views.report),
+    path("ge/search/", views.search),
+    path("ge/edit/<int:item_id>", views.edit_librarydata, name="edit_librarydata"),
     path("logs/", views.show_log, name="show_log"),
     path("logs/<int:line_count>", views.show_log, name="show_log"),
 ]
