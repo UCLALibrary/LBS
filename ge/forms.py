252382df057a474d1e27d4e175bd3bbaf1f30327
Zoe Tucker <zoetucker@library.ucla.edu>
2023-11-03 17:37:31 -0700

    working reports for all departments

--- ge/forms.py
+++ ge/forms.py
@@ -17,3 +17,34 @@ class ExcelUploadForm(forms.Form):
         help_text="Data from UCLA Monetary Transfer Form system",
         widget=forms.FileInput(),
     )
+
+
+class ReportForm(forms.Form):
+    report_type = forms.ChoiceField(
+        label="Report Type:",
+        choices=[
+            ("master", "Master"),
+            ("archives", "Archives"),
+            ("arts", "Arts"),
+            ("biomed", "Biomed"),
+            ("digilib", "Digital Library"),
+            ("eal", "East Asian Library"),
+            ("hsc", "History & SC Sciences"),
+            ("hssd", "HSSD"),
+            ("ias", "Intl & Area Studies"),
+            ("lhr", "LHR"),
+            ("lsc", "LSC"),
+            ("management", "Management"),
+            ("music", "Music"),
+            ("oh", "Oral History"),
+            ("pa", "Performing Arts"),
+            ("powell", "Powell"),
+            ("preservation", "Preservation"),
+            ("sel", "SEL"),
+            ("ul", "UL"),
+            ("aul_benedetti", "AUL Benedetti"),
+            ("aul_consales", "AUL Consales"),
+            ("aul_grappone", "AUL Grappone"),
+        ],
+        widget=forms.Select(),
+    )
