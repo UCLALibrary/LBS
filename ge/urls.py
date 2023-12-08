d88b3a32f4522889781eb7b5f2c34d285b626d9c
Zoe Tucker <zoetucker@library.ucla.edu>
2023-12-08 13:58:55 -0800

    add delete fund functionality

--- ge/urls.py
+++ ge/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
     path("", views.report),
     path("ge/report/", views.report),
-    path("ge/search/", views.search),
+    path("ge/search/", views.search, name="search"),
     path("ge/edit_fund/<int:item_id>", views.edit_fund, name="edit_fund"),
     path("ge/add_fund/", views.add_fund, name="add_fund"),
+    path("ge/delete_fund/<int:item_id>", views.delete_fund, name="delete_fund"),
     path("logs/", views.show_log, name="show_log"),
     path("logs/<int:line_count>", views.show_log, name="show_log"),
     path("release_notes/", views.release_notes, name="release_notes"),
