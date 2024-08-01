6060193adc6d3b4fa2d0d9d1de7d9a6eced26437
Andy Kohler <akohler@library.ucla.edu>
2024-08-01 15:27:30 -0800

    Reformat qdb/forms.py with Black

--- qdb/forms.py
+++ qdb/forms.py
@@ -1,10 +1,20 @@
-
 from django import forms
 from .models import Unit
 import datetime
+
 MONTHS = [
-    ('1', 'Jan'), ('2', 'Feb'), ('3', 'Mar'), ('4', 'Apr'), ('5', 'May'), ('6', 'Jun'), ('7',
-                                                                                         'Jul'), ('8', 'Aug'), ('9', 'Sep'), ('10', 'Oct'), ('11', 'Nov'), ('12', 'Dec'),
+    ("1", "Jan"),
+    ("2", "Feb"),
+    ("3", "Mar"),
+    ("4", "Apr"),
+    ("5", "May"),
+    ("6", "Jun"),
+    ("7", "Jul"),
+    ("8", "Aug"),
+    ("9", "Sep"),
+    ("10", "Oct"),
+    ("11", "Nov"),
+    ("12", "Dec"),
 ]
 currentDateTime = datetime.datetime.now()
 date = currentDateTime.date()
@@ -21,16 +31,13 @@ elif month_now > 1 and month_now < 13:
 
 YEARS = [(year, year)]
 for i in range(1, 3):
-    YEARS.append((year-i, year-i))
+    YEARS.append((year - i, year - i))
 YEARS = tuple(YEARS)
 
 
 class ReportForm(forms.Form):
-    unit = forms.ModelChoiceField(
-        queryset=Unit.objects.all().order_by('name'))
-    year = forms.ChoiceField(
-        choices=YEARS, initial=year_default)
-    month = forms.ChoiceField(
-        choices=MONTHS, initial=month_default)
+    unit = forms.ModelChoiceField(queryset=Unit.objects.all().order_by("name"))
+    year = forms.ChoiceField(choices=YEARS, initial=year_default)
+    month = forms.ChoiceField(choices=MONTHS, initial=month_default)
     send_email = forms.BooleanField(required=False, initial=True)
     override_recipients = forms.CharField(required=False)
