63b42a4130cc32a869e7c1c65bba3a781aeb298c
Andy Kohler <akohler@library.ucla.edu>
2024-12-04 18:45:35 -0900

    Add cronjob model/form/url/view

--- qdb/models.py
+++ qdb/models.py
@@ -14,7 +14,7 @@ class Staff(models.Model):
 
 class Unit(models.Model):
     name = models.CharField(max_length=100)
-    members = models.ManyToManyField(Staff, through='Recipient')
+    members = models.ManyToManyField(Staff, through="Recipient")
 
     def __str__(self):
         return self.name
@@ -47,3 +47,32 @@ class Account(models.Model):
 
     def __str__(self):
         return self.account
+
+
+class CronJob(models.Model):
+    # Use charfield to support wildcards and intervals.
+    # No attempt at validation.
+    # Set reasonable defaults for midnight Jan 1 (any day of week),
+    # basic echo command, with job disabled.
+    # 0 0 1 1 * echo 'Hello' >> /tmp/cron.log 2>&1
+    minutes = models.CharField(max_length=20, null=False, blank=False, default="0")
+    hours = models.CharField(max_length=20, null=False, blank=False, default="0")
+    days_of_month = models.CharField(
+        max_length=20, null=False, blank=False, default="1"
+    )
+    months = models.CharField(max_length=20, null=False, blank=False, default="1")
+    days_of_week = models.CharField(max_length=20, null=False, blank=False, default="*")
+    command = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        default="echo 'Hello' >> /tmp/cron.log 2>&1",
+    )
+    enabled = models.BooleanField(null=False, default=False)
+    # With custom save(), ensure there's only one record, with pk = 1:
+    # each new record replaces the previous (and only) one.
+    permanent_id = models.SmallIntegerField(default=1)
+
+    def save(self, *args, **kwargs):
+        self.pk = self.permanent_id
+        super().save(*args, **kwargs)
