382e657348052c63abb6b281b2f64bd478304652
DRickard <drickard1967@library.ucla.edu>
2026-07-30 10:21:36 -0700

    mapping from LibraryData to geFund, foreign key assignment

--- ge/management/commands/copy_funds.py
+++ ge/management/commands/copy_funds.py
@@ -1,19 +1,23 @@
 from django.core.management.base import BaseCommand
-from ge.models import GeFund, GeStaff
-from qdb.models import  Account, Recipient
+from ge.models import GeFund, GeStaff, LibraryData
 
 
 def populate_funds(self):
-    acct_records = Account.objects.all()
-    
+    acct_records = LibraryData.objects.all()
+
     fund_instances = [
         GeFund(
-            account=record.account,
-            cost_center=record.cost_center,
-            #fund=?,
-            title=record.title[:100],
-            manager=get_manager(record.unit),
-            mtf_authority=get_mtf_auth(record.unit)
+            account=record.fau_account,
+            cost_center=record.fau_cost_center,
+            fund=record.fau_fund,
+            title=record.fund_title,
+            manager=get_manager(record.fund_manager),
+            mtf_authority=get_mtf_auth(record.mtf_authority),
+            fund_purpose=record.fund_purpose,
+            fund_summary=record.fund_summary,
+            fund_restriction=record.fund_restriction,
+            general_notes=record.notes,
+            lbs_notes=record.lbs_notes
         )
         for record in acct_records
     ]
@@ -21,12 +25,12 @@ def populate_funds(self):
     GeFund.objects.bulk_create(fund_instances)
 
 
-def get_manager(mgr_unit):
-    return Recipient.objects.filter(unit=mgr_unit).filter(role="Head")
+def get_manager(mgr_name):
+    return GeStaff.objects.filter(name=mgr_name).first()
 
 
-def get_mtf_auth(mtf_auth_unit):
-    return Recipient.objects.filter(unit=mtf_auth_unit).filter(role="AUL")
+def get_mtf_auth(mtf_auth_name):
+    return GeStaff.objects.filter(name=mtf_auth_name).first()
 
 
 class Command(BaseCommand):
@@ -35,4 +39,3 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         populate_funds(self)
-
