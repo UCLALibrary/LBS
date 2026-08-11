01ae3edd971403487723427e10c3aeba7c2cab85
David Rickard <drickard1967@library.ucla.edu>
2026-08-11 09:53:21 -0700

    DLSR-517: Add script for exporting from models.LibraryData (#105)

---
+++ ge/management/commands/export_librarydata.py
@@ -0,0 +1,52 @@
+from django.core.management.base import BaseCommand
+from ge.models import LibraryData
+import csv
+
+
+def export_library_data():
+    fields_list = [
+        "unit",
+        "home_unit_dept",
+        "fund_title",
+        "fund_type",
+        "reg_fdn",
+        "fund_manager",
+        "ucop_fdn_no",
+        "fau_fund_no",
+        "fau_account",
+        "fau_cost_center",
+        "fau_fund",
+        "ytd_appropriation",
+        "ytd_expenditure",
+        "commitments",
+        "operating_balance",
+        "max_mtf_trf_amt",
+        "total_balance",
+        "mtf_authority",
+        "total_fund_value",
+        "projected_annual_income",
+        "fund_summary",
+        "fund_purpose",
+        "notes",
+        "home_dept",
+        "fund_restriction",
+        "new_fund",
+        "lbs_notes",
+    ]
+    library_data_records = (
+        LibraryData.objects.all().order_by("unit").values_list(*fields_list)
+    )
+
+    with open("library_data_export.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(fields_list)
+
+        for record in library_data_records:
+            writer.writerow(record)
+
+
+class Command(BaseCommand):
+    help = "Exports librarydata records to a CSV file."
+
+    def handle(self, *args, **options):
+        export_library_data()
