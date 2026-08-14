90414d83aa89323deff6e1c236a21751bcf7bc11
DRickard <drickard1967@library.ucla.edu>
2026-08-14 11:05:11 -0700

    add unit to displayed fund fields, linter formatting update

--- ge/admin.py
+++ ge/admin.py
@@ -1,27 +1,41 @@
 from django.contrib import admin
 from .models import GeStaff, GeUnit, GeFund
 
+
 # Register your models here.
 # create page to display GE staff
 @admin.register(GeStaff)
 class GeStaffAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email')
-    ordering = ('name',)
-    search_fields = ('name', 'email')
+    list_display = ("name", "email")
+    ordering = ("name",)
+    search_fields = ("name", "email")
 
 
 # create page to display GE units
 @admin.register(GeUnit)
 class GeUnitAdmin(admin.ModelAdmin):
-    list_display = ['name']
-    ordering = ('name',)
-    search_fields = ['name']
+    list_display = ["name"]
+    ordering = ("name",)
+    search_fields = ["name"]
 
 
 # create page to display GE funds
 @admin.register(GeFund)
 class GeFundAdmin(admin.ModelAdmin):
-    list_display = ('account', 'cost_center', 'fund', 'title', 'manager', 'mtf_authority', 'fund_purpose', 'fund_summary', 'fund_restriction', 'general_notes', 'lbs_notes')
-    ordering = ('account',)
-    search_fields = ('account', 'fund', 'title')
+    list_display = (
+        "account",
+        "cost_center",
+        "fund",
+        "title",
+        "manager",
+        "mtf_authority",
+        "unit",
+        "fund_purpose",
+        "fund_summary",
+        "fund_restriction",
+        "general_notes",
+        "lbs_notes",
+    )
+    ordering = ("account",)
+    search_fields = ("account", "fund", "title")
     list_filter = ["manager", "mtf_authority"]
