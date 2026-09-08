43329b196337fea823d21f9f2aa1e8c99de9d960
Andy Kohler <akohler@library.ucla.edu>
2026-09-08 14:20:44 -0800

    Remove temporary QDB query view and URL

--- ge/urls.py
+++ ge/urls.py
@@ -11,6 +11,4 @@ urlpatterns = [
     path("logs/", views.show_log, name="show_log"),
     path("logs/<int:line_count>", views.show_log, name="show_log"),
     path("release_notes/", views.release_notes, name="release_notes"),
-    # TODO: Change this temporary path & view name, when original report is fully replaced.
-    path("ge/report_from_qdb_temp/", views.report_from_qdb_temp),
 ]
